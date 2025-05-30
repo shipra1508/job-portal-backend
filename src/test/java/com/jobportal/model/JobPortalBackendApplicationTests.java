0e105282aee9da6e1b4ebd3d008349031215c05f
Shipra <shiprashikta@gmail.com>
2025-05-29 23:45:44 -0500

    add entity and repo

---
+++ src/test/java/com/jobportal/model/JobPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
